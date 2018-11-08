108f6e86caf31dd227ea6ce4c296377c1e24376f
angelsol299 <angelsol299@hotmail.com>
2018-11-08 20:45:16 +0100

    main page and subpages
    styling added as well and some content

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
 
 class LandingPage extends Component {
   render() {
     return (
-      <div>
-        <h1>Landing</h1>
+      <div style={{ width: "100%", margin: "auto" }}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img
+              src="https://images.vexels.com/media/users/3/129616/isolated/preview/fb517f8913bd99cd48ef00facb4a67c0-businessman-avatar-silhouette-by-vexels.png"
+              alt="avatar"
+              className="avatar-img"
+            />
+            <div className="banner-text">
+              <h1>Front End Developer</h1>
+              <hr />
+            </div>
+          </Cell>
+        </Grid>
       </div>
     );
   }
