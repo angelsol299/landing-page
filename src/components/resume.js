abe46d4b8caa6ef5b68abe9e24f53046ca4ac162
angelsol299 <angelsol299@hotmail.com>
2018-11-10 22:48:16 +0100

    two components for resume
    js components with corresponding styling

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Grid, Cell } from "react-mdl";
+import Education from "./education";
+import Experience from "./experience";
 
 class Resume extends Component {
   render() {
@@ -13,20 +15,55 @@ class Resume extends Component {
                 alt="avatar"
                 style={{ height: "200px" }}
               />
-              <h2 style={{ paddingTop: "2em" }}>Angel Osoria</h2>
-              <h4 style={{ color: "grey" }}>Front End Developer</h4>
-              <hr style={{ borderTop: "3px solid #833fb2", width: "50%" }} />
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Repellendus illum iste, recusandae, facilis obcaecati iusto
-                numquam quo culpa incidunt vel nostrum quam eligendi! Eligendi
-                aperiam quia aspernatur enim suscipit error.
-              </p>
-              <hr style={{ borderTop: "3px solid #833fb2", width: "50%" }} />
             </div>
+            <h2 style={{ paddingTop: "2em" }}>Angel Osoria</h2>
+            <h4 style={{ color: "grey" }}>Front End Developer</h4>
+            <hr style={{ borderTop: "3px solid #833fb2", width: "50%" }} />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Repellendus illum iste, recusandae, facilis obcaecati iusto
+              numquam quo culpa incidunt vel nostrum quam eligendi! Eligendi
+              aperiam quia aspernatur enim suscipit error.
+            </p>
+            <hr style={{ borderTop: "3px solid #833fb2", width: "50%" }} />
+            <h5>Address</h5>
+            <p>Norra statationsgatan, Stockholm</p>
+            <h5>Phone</h5>
+            <p>3314050542</p>
+            <h5>Email</h5>
+            <p>info@angelosoria.com</p>
+            <h5>Web</h5>
+            <p>www.angelosoria.com</p>
+            <hr style={{ borderTop: "3px solid #833fb2", width: "50%" }} />
           </Cell>
           <Cell className="resume-right-col" col={8}>
-            Right side
+            <h2>Education</h2>
+            <Education
+              startYear={2017}
+              endYear={2019}
+              schoolName={"Nackademin"}
+              schoolDescription={
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit Repellendus illum iste, recusandae, facilis obcaecati iusto"
+              }
+            />
+            <hr style={{ borderTop: "3px solid #e22947" }} />
+            <h2>Experience</h2>
+            <Experience
+              startYear={2018}
+              endYear={2018}
+              jobName={"Front End Developer Intern"}
+              jobDescription={
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit Repellendus illum iste, recusandae, facilis obcaecati iusto"
+              }
+            />
+            <Experience
+              startYear={2018}
+              endYear={2018}
+              jobName={"Front End Developer Intern"}
+              jobDescription={
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit Repellendus illum iste, recusandae, facilis obcaecati iusto"
+              }
+            />
           </Cell>
         </Grid>
       </div>
