be6f31a67a8572eb71506de45da62016470fe756
angelsol299 <angelsol299@hotmail.com>
2018-11-12 19:51:00 +0100

    fixeda
    deleted files by accident

---
+++ src/components/aboutMe.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class About extends Component {
+  render() {
+    return (
+      <div style={{ marginTop: "70px" }}>
+        <h1>About Page</h1>
+      </div>
+    );
+  }
+}
+
+export default About;
