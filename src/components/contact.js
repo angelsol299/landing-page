e7f60eb0e23981c42e832eb9ad46b53975f83c7d
angelsol299 <angelsol299@hotmail.com>
2018-11-16 23:48:21 +0100

    fixing contact page
    adding images and buttons

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,8 +4,19 @@ import { Grid, Cell, List, ListItem, ListItemContent } from "react-mdl";
 class Contact extends Component {
   render() {
     return (
-      <div className="contact-body" style={{ marginTop: "70px" }}>
-        <Grid className="contact-grid">
+      <div className="contact-body" style={{ marginTop: "30px" }}>
+        <div class="container">
+          <img
+            src="https://wallpapertag.com/wallpaper/full/7/1/f/248315-gorgerous-coding-background-1920x1080.jpg"
+            alt="Snow"
+            style={{ width: "100%" }}
+          />
+          <button href="#contact" class="btn">
+            Contact me
+          </button>
+        </div>
+
+        <Grid className="contact-grid" id="contact">
           <Cell col={6}>
             <div class="cognito" />
           </Cell>
