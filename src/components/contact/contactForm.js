3a0000404256e9a10aeee5607a478ac3cb624b25
angelsol299 <angelsol299@hotmail.com>
2018-11-18 23:41:40 +0100

    contact form
    format done

--- src/components/contact/contactForm.js
+++ src/components/contact/contactForm.js
@@ -4,8 +4,36 @@ class ContactForm extends Component {
   render() {
     return (
       <div>
-        <label htmlFor="">Name:</label>
+        <h2>Contact me</h2>
+        <label className="formLabel" htmlFor="">
+          Name:
+        </label>
         <input type="text" />
+        <br />
+        <br />
+        <label className="formLabel" htmlFor="">
+          Email:
+        </label>
+        <input type="text" />
+        <br />
+        <br />
+        <label className="formLabel" htmlFor="">
+          Phone:
+        </label>
+        <input type="text" />
+        <br />
+        <br />
+        <label
+          className="formLabel"
+          htmlFor=""
+          style={{ marginLeft: "-140px" }}
+        >
+          Content:
+          <br />
+        </label>
+        <textarea name="" id="" cols="30" rows="10" />
+        <br />
+        <button style={{}}>Send</button>
       </div>
     );
   }
